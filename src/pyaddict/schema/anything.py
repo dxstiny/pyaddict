c13389c181d975d4b8172ecfcc61df5238ecb5b5
davidohnee <davidohnee@gmail.com>
2026-07-05 12:36:13 +0200

    anything: make nullable

--- src/pyaddict/schema/anything.py
+++ src/pyaddict/schema/anything.py
@@ -1,14 +1,25 @@
-from typing import Any
+from typing import Any, cast
 
 from pyaddict.schema.base import ISchemaType
 from pyaddict.schema.result import ValidationResult
 
 
-class Anything(ISchemaType["Anything", Any | None]):
-    def nullable(self) -> Any:
-        return self
+class _Anything[R](ISchemaType["_Anything[R]", R]):
+    def nullable(self) -> "_Anything[R | None]":
+        return cast(OptionalAnything, super().nullable())
 
     def validate(
         self, value: Any | None, *, path: list[str] | None = None
     ) -> ValidationResult[Any | None]:
+        path = path or []
+
+        result = self._test_nullable(value, path)
+
+        if not result or result.value is None:
+            return cast(ValidationResult[R], result)
+
         return ValidationResult.ok(value)
+
+
+Anything = _Anything[Any]
+OptionalAnything = _Anything[Any | None]
